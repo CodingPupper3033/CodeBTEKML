b5f4d284fffb6687fed94f3113d6561284dd084e
Joshua Miller <codingpupper3033@gmail.com>
2022-11-09 13:28:48 -0500

    Updating to v1.1.0

--- src/main/java/com/codingpupper3033/codebtekml/CodeBTEKMLMod.java
+++ src/main/java/com/codingpupper3033/codebtekml/CodeBTEKMLMod.java
@@ -17,7 +17,7 @@ public class CodeBTEKMLMod
 {
     public static final String MODID = "codebtekml";
     public static final String NAME = "BTE KML Importer";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.1.0";
 
     private static Logger logger;
 
