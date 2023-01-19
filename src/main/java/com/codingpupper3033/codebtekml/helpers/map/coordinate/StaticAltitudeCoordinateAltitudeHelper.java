c84a1c4698cc1a626fe11e76f709af6467cdeb9e
Joshua Miller <codingpupper3033@gmail.com>
2023-01-18 22:17:55 -0500

    First attempt at a static elevation

---
+++ src/main/java/com/codingpupper3033/codebtekml/helpers/map/coordinate/StaticAltitudeCoordinateAltitudeHelper.java
@@ -0,0 +1,29 @@
+package com.codingpupper3033.codebtekml.helpers.map.coordinate;
+
+import com.codingpupper3033.codebtekml.helpers.map.altitude.AltitudeMode;
+
+import javax.naming.OperationNotSupportedException;
+import java.util.Objects;
+
+/**
+ * @author Joshua Miller
+ */
+public class StaticAltitudeCoordinateAltitudeHelper extends CoordinateAltitudeHelper {
+    public int staticAltitude;
+
+    public StaticAltitudeCoordinateAltitudeHelper(int staticAltitude) {
+        this.staticAltitude = staticAltitude;
+    }
+
+    @Override
+    public void convertCoordinatesAltitudeMode(Coordinate[] coordinates, AltitudeMode convertTo) throws OperationNotSupportedException {
+        if (Objects.requireNonNull(convertTo) == AltitudeMode.ABSOLUTE) {
+            for (Coordinate coordinate : coordinates) {
+                coordinate.setElevation(staticAltitude);
+                coordinate.setAltitudeMode(AltitudeMode.ABSOLUTE);
+            }
+        } else {
+            throw new OperationNotSupportedException();
+        }
+    }
+}
