437d379529774b27a66333a8f63b2a1932c0edca
Joshua Miller <codingpupper3033@gmail.com>
2023-01-18 22:05:57 -0500

    Added debug mode to CommandSender.java. This allows for easier debugging of the commands sent. Could be sent to a text file in the future.

--- src/main/java/com/codingpupper3033/codebtekml/mapdrawer/CommandSender.java
+++ src/main/java/com/codingpupper3033/codebtekml/mapdrawer/CommandSender.java
@@ -1,6 +1,8 @@
 package com.codingpupper3033.codebtekml.mapdrawer;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -11,6 +13,7 @@ import java.io.PrintStream;
  * @author Joshua Miller
  */
 public class CommandSender {
+    static final boolean DEBUG_MODE = false;
     static PrintStream out = new PrintStream(new CommandOutputStream());
 
     private static class CommandOutputStream extends OutputStream {
@@ -29,7 +32,12 @@ public class CommandSender {
         private void output() {
             String command = buffer.toString();
             command = command.replace("minecraft:",""); // World Edit doesn't know what minecraft: is ._.
-            Minecraft.getMinecraft().player.sendChatMessage(command);
+
+            if (!DEBUG_MODE) {
+                Minecraft.getMinecraft().player.sendChatMessage(command);
+            } else {
+                Minecraft.getMinecraft().player.sendMessage(new TextComponentString(command.replaceAll("\r","")));
+            }
 
             buffer = new StringBuffer();
         }
